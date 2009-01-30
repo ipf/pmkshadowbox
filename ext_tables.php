4b120faa30374d3fc96e213baa895342e5684985
pmk65 <pmk65@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-30 11:38:12 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkshadowbox/trunk@16219 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/PMK_Shadowbox/', 'PMK Shadowbox');
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/PMK_ShadowboxXHTML/', 'PMK Shadowbox (xhtml)');
+?>
