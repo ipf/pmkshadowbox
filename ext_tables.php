2a381010ac7bee7c18f2eda0aab6c96f0a1a341a
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-05 14:45:51 +0000

    - merged static templates - removed user function class - several cleanups
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkshadowbox/trunk@16494 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/PMK_Shadowbox/', 'PMK Shadowbox');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/PMK_ShadowboxXHTML/', 'PMK Shadowbox (xhtml)');
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/PMK_Shadowbox/', 'PMK Shadowbox');
 ?>
