f79cee44a73d32bccd3b1ff5e15fa802d25fd738
pmk65 <pmk65@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-26 15:27:27 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkshadowbox/trunk@19484 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -4,4 +4,6 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/PMK_Shadowbox/', 'PMK Shadowbox');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/PMK_Shadowbox_tt_news/', 'PMK Shadowbox tt_news');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/PMK_Shadowbox_tt_products/', 'PMK Shadowbox tt_products');
 ?>
