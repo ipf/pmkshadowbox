80cfd836da289f0002edb86634846b5f9a581b47
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-27 11:34:19 +0000

    just some code cleanups and formatting of the manual
    Note: Some images were lost inside the manual with OOo 3.0. They should be inserted again! Sorry!
    
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkshadowbox/trunk@19512 735d13b6-9817-0410-8766-e36946ffe9aa

--- tt_news_imageMarkerFunc.php
+++ tt_news_imageMarkerFunc.php
@@ -24,27 +24,31 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
+
 /**
- * Sets the register var "IMAGE_NUM_CURRENT" to the the current image number, similar to tt_content.
+ * Sets the register var "IMAGE_NUM_CURRENT" to the
+ * the current image number, similar to tt_content.
  *
- * @author    Peter Klein <peter@umloud.dk>
+ * @author	Peter Klein <peter@umloud.dk>
  */
 
 /**
- * Sets the register var "IMAGE_NUM_CURRENT" to the the current image number, similar to tt_content.
+ * Sets the register var "IMAGE_NUM_CURRENT" to the the current
+ * image number, similar to tt_content.
  *
- * @param    array        $paramArray: $markerArray and $config of the current news item in an array
- * @param    [type]        $conf: ...
- * @return    array        the processed markerArray
+ * @param	array	$paramArray $markerArray and $config of the current news item in an array
+ * @param	array	$conf configuration
+ * @return	array	the processed markerArray
  */
-function user_imageMarkerFunc($paramArray,$conf){
+function user_imageMarkerFunc($paramArray, $conf) {
+	// make a reference to the parent-object
+	$pObj = &$conf['parentObj'];
 
 	$markerArray = $paramArray[0];
 	$lConf = $paramArray[1];
-	$pObj = &$conf['parentObj']; // make a reference to the parent-object
 	$row = $pObj->local_cObj->data;
 
-	$imageNum = isset($lConf['imageCount']) ? $lConf['imageCount']:1;
+	$imageNum = isset($lConf['imageCount']) ? $lConf['imageCount'] : 1;
 	$imageNum = t3lib_div::intInRange($imageNum, 0, 100);
 	$theImgCode = '';
 	$imgs = t3lib_div::trimExplode(',', $row['image'], 1);
@@ -53,30 +57,47 @@ function user_imageMarkerFunc($paramArray,$conf){
 	$cc = 0;
 
 	while (list(, $val) = each($imgs)) {
-		if ($cc == $imageNum) break;
+		if ($cc == $imageNum) {
+			break;
+		}
+
 		if ($val) {
-			 $lConf['image.']['altText'] = ''; // reset altText
-			$lConf['image.']['altText'] = $lConf['image.']['altText']; // set altText to value from TS
-			$lConf['image.']['file'] = 'uploads/pics/'.$val;
+			// reset altText
+			$lConf['image.']['altText'] = '';
+
+			// set altText to value from TS
+			$lConf['image.']['altText'] = $lConf['image.']['altText'];
+
+			$lConf['image.']['file'] = 'uploads/pics/' . $val;
 			switch($lConf['imgAltTextField']) {
 				case 'image':
 					$lConf['image.']['altText'] .= $val;
-				break;
+					break;
+
 				case 'imagecaption':
 					$lConf['image.']['altText'] .= $imgsCaptions[$cc];
-				break;
+					break;
+
 				default:
 					$lConf['image.']['altText'] .= $row[$lConf['imgAltTextField']];
+					break;
 			}
 		}
+
 		$GLOBALS['TSFE']->register['IMAGE_NUM_CURRENT'] = $cc;
-		$theImgCode .= $pObj->local_cObj->IMAGE($lConf['image.']).$pObj->local_cObj->stdWrap($imgsCaptions[$cc], $lConf['caption_stdWrap.']);
-		$cc++;
+		$theImgCode .= $pObj->local_cObj->IMAGE($lConf['image.']) .
+			$pObj->local_cObj->stdWrap($imgsCaptions[$cc], $lConf['caption_stdWrap.']);
+		++$cc;
 	}
+
 	$markerArray['###NEWS_IMAGE###'] = '';
 	if ($cc) {
-		$markerArray['###NEWS_IMAGE###'] = $pObj->local_cObj->wrap(trim($theImgCode),$lConf['imageWrapIfAny']);
+		$markerArray['###NEWS_IMAGE###'] = $pObj->local_cObj->wrap(
+			trim($theImgCode),
+			$lConf['imageWrapIfAny']
+		);
 	}
+
 	return $markerArray;
 }
 
