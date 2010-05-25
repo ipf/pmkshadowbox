dbd8a27cc380fabe4f2b66ab3119532027be854c
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-25 18:24:10 +0000

    The extension was completely refactored and so we need to remove anything.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkshadowbox/trunk@33460 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-
-t3lib_extMgm::addStaticFile($_EXTKEY, 'static/PMK_Shadowbox/', 'PMK Shadowbox');
-t3lib_extMgm::addStaticFile($_EXTKEY, 'static/PMK_Shadowbox_tt_news/', 'PMK Shadowbox tt_news');
-t3lib_extMgm::addStaticFile($_EXTKEY, 'static/PMK_Shadowbox_tt_products/', 'PMK Shadowbox tt_products');
-?>
