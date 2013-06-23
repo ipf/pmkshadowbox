9bb29470d6de24f04beb6f0f1de12709bb8118e6
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-06-23 21:40:35 +0200

    Fix for autoloader (#49363 in forge)

--- ext_autoload.php
+++ ext_autoload.php
@@ -4,7 +4,7 @@ $extensionPath = t3lib_extMgm::extPath('pmkshadowbox');
 return array(
 	'tx_pmkshadowbox_cache' => $extensionPath . 'classes/class.tx_pmkshadowbox_cache.php',
 	'tx_pmkshadowbox_build' => $extensionPath . 'classes/class.tx_pmkshadowbox_build.php',
-	'tx_ttnews_imageMarker' => $extensionPath . 'classes/class.tx_ttnews_imageMarker.php',
+	'tx_ttnews_imageMarkerHook' => $extensionPath . 'classes/class.tx_ttnews_imageMarkerHook.php',
 );
 
 ?>
